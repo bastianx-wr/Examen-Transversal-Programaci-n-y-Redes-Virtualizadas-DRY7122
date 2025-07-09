7f4917bb1b49d9cb0f39fada22eedb671cf49589
NetAcad DEVASC <changeme@example.com>
2025-07-09 02:14:42 +0000

    Agregados scripts de Python para el Item 1 del examen

---
+++ python/# grupo_info.py
@@ -0,0 +1,11 @@
+# grupo_info.py
+
+integrantes_del_grupo = [
+    "Carlos Montiel",
+    "Alexis Suarez"
+]
+
+print("--- Integrantes del Grupo ---")
+for integrante in integrantes_del_grupo:
+    print(integrante)
+print("----------------------------")
